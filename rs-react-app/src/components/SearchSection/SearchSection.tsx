1f74bcdfa7b281ddcf9599b9d04df2db2a06fb3f
Katya <kate.halitsa@gmail.com>
2026-04-29 19:06:22 +0300

    feat: implement search functionality with local storage

--- rs-react-app/src/components/SearchSection/SearchSection.tsx
+++ rs-react-app/src/components/SearchSection/SearchSection.tsx
@@ -1,13 +1,36 @@
 import { Component, type ReactNode } from "react";
 import './SearchSection.css';
-class SearchSection extends Component{
+const SEARCH_STORAGE_KEY = "rssSearch";
+type State = {
+  lastSearch: string;
+};
+class SearchSection extends Component<{}, State>{
+    constructor(props: {}) {
+    super(props);
+    this.state = {lastSearch: localStorage.getItem(SEARCH_STORAGE_KEY)|| '',};
+    }
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    this.setState({
+        lastSearch: value,
+    });
+
+    localStorage.setItem(
+        SEARCH_STORAGE_KEY,
+        value
+    );
+    };
+        
     render(): ReactNode {
         return(
             <section className="search-section">
                 <div className="search-wapper">
                     <input className="search-input"
                     type="text"
-                    placeholder="Search...">
+                    placeholder="Search..."
+                    value = {this.state.lastSearch}
+                    onChange={this.handleChange}>
                     </input>
                     <button className="search-btn">
                         Search
