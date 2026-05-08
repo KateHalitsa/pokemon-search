2c8b7941fc3edb3213114e64a4e5396411605d2c
Katya <kate.halitsa@gmail.com>
2026-05-08 15:07:13 +0300

    chore: setup jsdom environment for vitest

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/katehalitsa-REACT2026Q2/"
+  base:"/pokemon-search/",
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{ts,tsx}',
+        'src/**/*.spec.{ts,tsx}',
+        'src/main.tsx'
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 })
