15e9b9d3c79cd6b84d4bb81a9cc405050eb4e4df
Katya <kate.halitsa@gmail.com>
2026-06-06 19:24:35 +0300

    feat: implement modal with react portals

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-
-import './App.css'
+import { useState } from "react";
+import Modal from "./components/Modal/Modal";
+import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-    Заготовка
-       </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <button onClick={() => setIsOpen(true)}>
+          Click to Open Modal
+        </button>
+
+        <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
+          This is Modal Content!
+        </Modal>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
